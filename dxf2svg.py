581889ad10646fc6c0e37faa24a0e513c3a34f01
ducky <richard.lin@berkeley.edu>
2016-07-13 14:30:46 -0700

    Make it work on latest dxfgrabber, add units support, add defined cut stroke and color

--- dxf2svg.py
+++ dxf2svg.py
@@ -3,12 +3,14 @@
 import dxfgrabber
 import math
 import sys
+import os
 
 # SVG TEMPLATES
 
+# TODO: support arbitrary, user-specifiable units
 SVG_PREAMBLE = \
 '<svg xmlns="http://www.w3.org/2000/svg" ' \
-'version="1.1" viewBox="{0} {1} {2} {3}">\n'
+'version="1.1" viewBox="{0} {1} {2} {3}" width="{4}in" height="{5}in">\n'
 
 # SVG_MOVE_TO = 'M {0} {1:.2f} '
 # SVG_LINE_TO = 'L {0} {1:.2f} '
@@ -19,13 +21,19 @@ SVG_LINE_TO = 'L {0} {1} '
 SVG_ARC_TO  = 'A {0} {1} {2} {3} {4} {5} {6} '
 
 SVG_PATH = \
-'<path d="{0}" fill="none" stroke="{1}" stroke-width="{2:.2f}" />\n'
+'<path d="{0}" fill="none" stroke="{1}" stroke-width="{2}" />\n'
 
 SVG_LINE = \
-'<line x1="{0}" y1="{1}" x2="{2}" y2="{3}" stroke="{4}" stroke-width="{5:.2f}" />\n'
+'<line x1="{0}" y1="{1}" x2="{2}" y2="{3}" stroke="{4}" stroke-width="{5}" />\n'
 
 SVG_CIRCLE = \
 '<circle cx="{0}" cy="{1}" r="{2}" stroke="{3}" stroke-width="{4}" fill="none" />\n'
+
+# from http://www.w3.org/TR/SVG/coords.html#Units
+CUT_STROKE_COLOR = '#ff0000'
+CUT_STROKE_WIDTH = 0.001/(90)  # TODO: user-specified, arbitrary units
+
+ENGRAVE_STOKE_COLOR = '#000000'
 
 # SVG DRAWING HELPERS
 
@@ -37,13 +45,7 @@ def angularDifference(startangle, endangle):
     result += 360
   return result
 
-def moveTo(point):
-    return SVG_MOVE_TO.format(point.x,height-point.y)
-
-def lineTo(point):
-    return SVG_LINE_TO.format(point.x,height-point.y)
-
-def pathStringFromPoints(points):  
+def pathStringFromPoints(points):
   pathString = SVG_MOVE_TO.format(*points[0])
   for i in range(1,len(points)):
     pathString += SVG_LINE_TO.format(*points[i])
@@ -54,25 +56,30 @@ def pathStringFromPoints(points):
 def handleEntity(svgFile, e):
   # TODO: handle colors and thinckness
   # TODO: handle elipse and spline and some other types
-  
-  if isinstance(e, dxfgrabber.entities.Line):      
+
+  if isinstance(e, dxfgrabber.dxfentities.Line):
     svgFile.write(SVG_LINE.format(
       e.start[0], e.start[1], e.end[0], e.end[1],
-      'black', 1          
+      'black', 1
     ))
 
-  elif isinstance(e, dxfgrabber.entities.LWPolyline):
+  elif isinstance(e, dxfgrabber.dxfentities.LWPolyline):
     pathString = pathStringFromPoints(e)
     if e.is_closed:
       pathString += 'Z'
-    svgFile.write(SVG_PATH.format(pathString, 'black', 1))
-    
-  elif isinstance(e, dxfgrabber.entities.Circle):
+    svgFile.write(SVG_PATH.format(pathString, CUT_STROKE_COLOR, CUT_STROKE_WIDTH))
+
+  elif isinstance(e, dxfgrabber.dxfentities.Polyline):
+    pathString = pathStringFromPoints(e)
+    if e.is_closed:
+      pathString += 'Z'
+    svgFile.write(SVG_PATH.format(pathString, CUT_STROKE_COLOR, CUT_STROKE_WIDTH))
+
+  elif isinstance(e, dxfgrabber.dxfentities.Circle):
     svgFile.write(SVG_CIRCLE.format(e.center[0], e.center[1],
       e.radius, 'black', 1))
 
-  elif isinstance(e, dxfgrabber.entities.Arc):
-    
+  elif isinstance(e, dxfgrabber.dxfentities.Arc):
     # compute end points of the arc
     x1 = e.center[0] + e.radius * math.cos(math.pi * e.startangle / 180)
     y1 = e.center[1] + e.radius * math.sin(math.pi * e.startangle / 180)
@@ -83,28 +90,33 @@ def handleEntity(svgFile, e):
     pathString += SVG_ARC_TO.format(e.radius, e.radius, 0,
       int(angularDifference(e.startangle, e.endangle) > 180), 1, x2, y2)
 
-    svgFile.write(SVG_PATH.format(pathString, 'black', 1))
-  elif isinstance(e, dxfgrabber.entities.Insert):
-    # TODO: handle group instances
-    pass
+    svgFile.write(SVG_PATH.format(pathString, CUT_STROKE_COLOR, CUT_STROKE_WIDTH))
+  elif isinstance(e, dxfgrabber.dxfentities.Solid):
+    reordered_points = [e.points[0], e.points[1], e.points[3], e.points[2]]
+    pathString = pathStringFromPoints(reordered_points)
+    pathString += 'Z'
+    svgFile.write(SVG_PATH.format(pathString, CUT_STROKE_COLOR, CUT_STROKE_WIDTH))
+  else:
+    raise Exception("Unknown type %s" % e)
 #end: handleEntity
 
 def saveToSVG(svgFile, dxfData):
-  
+
   minX = dxfData.header['$EXTMIN'][0]
   minY = dxfData.header['$EXTMIN'][1]
   maxX = dxfData.header['$EXTMAX'][0]
   maxY = dxfData.header['$EXTMAX'][1]
-  
+
   # TODO: also handle groups
   svgFile.write(SVG_PREAMBLE.format(
-    minX, minY, maxX - minX, maxY - minY))
+    minX, minY, maxX - minX, maxY - minY,
+    abs(maxX - minX), abs(maxY - minY)))
 
   for entity in dxfData.entities:
     layer = dxfData.layers[entity.layer]
     if layer.on and not layer.frozen:
       handleEntity(svgFile, entity)
-     
+
   svgFile.write('</svg>\n')
 #end: saveToSVG
 
@@ -113,18 +125,20 @@ if __name__ == '__main__':
   if len(sys.argv) < 2:
     sys.exit('Usage: {0} file-name'.format(sys.argv[0]))
 
-  filename = sys.argv[1]
+  for filename in sys.argv[1:]:
+    # grab data from file
+    dxfData = dxfgrabber.readfile(filename)
 
-  # grab data from file
-  dxfData = dxfgrabber.readfile(filename)
+    # TODO: alret if the file already exist
+    # convert and save to svg
+    svgName = '.'.join(filename.split('.')[:-1] + ['svg'])
+    svgFile = open(svgName, 'w')
 
-  # TODO: alret if the file already exist
-  # convert and save to svg
-  svgName = '.'.join(filename.split('.')[:-1] + ['svg'])
-  svgFile = open(svgName, 'w')
+    label_basename = os.path.basename(filename).split('_')[0]
+    print("Opening '%s' (%s)" % (filename, label_basename))
 
-  saveToSVG(svgFile, dxfData)
+    saveToSVG(svgFile, dxfData)
 
-  svgFile.close()
+    svgFile.close()
 #end: __main__
 
